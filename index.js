4ffc336911c2ab85380cb2b12b14eba6dcc0747e
Lauramartinezsanchez <lauramartinezsanchez01@gmail.com>
2022-04-02 18:04:35 +0200

    update

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const cool = require("cool-ascii-faces");
+//const cool = require("cool-ascii-faces");
 const express = require("express");
 
 const app = express();
 const port= process.env.PORT || 8080;
 
 
-
+/*
 app.get("/caritas", (req,res)=>{
     console.log("Requested / route");
     res.send("<html><body><h1>" +cool()+ "</h1></body></html>")
@@ -15,6 +15,10 @@ app.get("/caritas", (req,res)=>{
 app.get("/time", (req,res)=>{
     console.log("Requested/time route");
     res.send("<html><body><h1>" + new Date() + "</h1></body></html>")
+});
+*/
+app.get("/api", (req,res)=>{
+    res.send({ result: true });
 });
 
 app.listen(port, ()=> {
